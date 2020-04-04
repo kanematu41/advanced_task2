c11cc5640b099042a54a9a35f453b142a765db03
unknown <grenson.crtis@gmail.com>
2020-04-04 22:25:35 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   resources :books
   root 'home#top'
   get 'home/about'
+
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+
+  get 'user/:id/follows' => 'relationships#follows', as: 'follows'
+  get 'user/:id/followers' => 'relationships#followers', as: 'followers'
 end
